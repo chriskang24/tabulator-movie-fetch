5ffbebec1689a637d92a431532bbd5f8f8428443
Christopher <christopherjkang24@gmail.com>
2021-05-11 16:57:54 -0400

    init commit with working tabulator movie fetch

---
+++ movie-data/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'react-tabulator/lib/styles.css';
+import MovieFetch from './Components/data-fetch/movie-fetch-component';
+
+function App() {
+  return (
+    <div>
+      <MovieFetch />
+    </div>
+  );
+}
+
+export default App;
